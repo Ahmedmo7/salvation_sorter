b0ef63a8102e2450f77dfd524e3ca3ba27128fc7
mkopanic <haydenhoffman@Haydens-MacBook-Air.local>
2018-11-24 19:21:41 -0500

    Added choose files and a bunch of other things

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,23 @@
 <template>
-  <v-app id="inspire" red>
+  <v-app id="inspire">
     <vs-sidebar parent="body" default-index="1"  color="dark" class="sidebarx" spacer v-model="drawer">
-
-      <vs-sidebar-item index="1" icon="home">
+      <vs-sidebar-item index="1" icon="home" to="/home">
+        Home
+      </vs-sidebar-item>
+      <vs-sidebar-item index="2" icon="home" to="/inspire">
         Quality
       </vs-sidebar-item>
 
-      <vs-sidebar-item index="2" icon="gavel">
+      <vs-sidebar-item index="3" icon="gavel" to="/main">
         Soiled
       </vs-sidebar-item>
 
     </vs-sidebar>
-    <v-toolbar app fixed clipped-left>
+    <v-toolbar app fixed clipped-left color="primary lighten-5">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Dashboard</v-toolbar-title>
     </v-toolbar>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <v-card>
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-                aspect-ratio="2.75"
-              ></v-img>
-
-              <v-card-title primary-title>
-                <div>
-                  <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                  <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-                </div>
-              </v-card-title>
-
-              <v-card-actions>
-                <v-btn flat color="orange">Share</v-btn>
-                <v-btn flat color="orange">Explore</v-btn>
-              </v-card-actions>
-            </v-card>
-            <v-tooltip right>
-              <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank">
-                <v-icon large>mdi-codepen</v-icon>
-              </v-btn>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+    <router-view></router-view>
   </v-app>
 </template>
 
---
+++ pages/home.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-parallax height="700" src="https://images.pexels.com/photos/994517/pexels-photo-994517.jpeg?auto=compress&cs=tinysrgb&h=650&w=940">
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <vs-popup class="" style="font-family: 'Arial'"  title="Upload Photos" :active.sync="popupActive">
+              <h3> Click choose file and upload a photo of your clothing! </h3>
+              <vs-input v-if="popupActive" type="file" @change="onFileSelected"></vs-input>
+            </vs-popup>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Welcome to Salvation Solver!</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip right>
+                  <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank">
+                    <v-icon large>mdi-codepen</v-icon>
+                  </v-btn>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-img id="image" src="https://raw.githubusercontent.com/Ahmedmo7/salvation_sorter/master/Ss_final_resize.jpg"/>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn @click="uploadPhoto" color="primary">Upload Photos</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn @click="capturePhoto" color="primary">Capture Photo</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      </v-parallax>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      popupActive: false
+    }),
+    methods: {
+      // Make sure these methods work
+      uploadPhoto() {
+        this.popupActive = true;
+      },
+      capturePhoto() {
+        this.popupActive = true;
+      }
+    },
+    props: {
+      source: String
+    }
+  }
+</script>
+
+<style scoped>
+</style>
